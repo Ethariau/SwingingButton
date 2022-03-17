afc419a36f11c1c2b827a3985883ec98edeb3be5
Ethariau <clem.debeaupuy@gmail.com>
2022-03-17 16:45:40 +0100

    final version

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
 const leftRound = document.querySelector(".leftRound");
 const rightRound = document.querySelector(".rightRound");
-const article = document.querySelectorAll("article")
-console.log(article);
-deg=0;
-article[0].addEventListener("mouseover", ()=>{
+const article = document.querySelectorAll("article");
+const leftText = document.querySelector(".leftSide .displayFluid");
+const rightText = document.querySelector(".rightSide .displayFluid");
+degLeft = 0;
+degRight = 0;
+leftText.style.display = "none";
+rightText.style.display = "none";
+
+console.log(article[0].style.display);
+article[0].addEventListener("mouseover", () => {
   if (article[0].classList == "leftSide") {
-    deg+=360;
-  leftRound.style.transform = 'rotate('+deg+'deg)';
-  article[0].classList.add("flexThree");
-  article[1].classList.remove("flexThree");
-  }else{
-    deg=deg-360;
-    leftRound.style.transform = 'rotate('+deg+'deg)';
-    article[0].classList.remove("flexThree");
-  }
-})
-article[1].addEventListener("mouseover", ()=>{
-  if (article[1].classList == "rightSide") {
-    deg=deg-360;
-  rightRound.style.transform = 'rotate('+deg+'deg)';
-  article[1].classList.add("flexThree");
-  article[0].classList.remove("flexThree");
-  }else{
-    deg+=360;
-    rightRound.style.transform = 'rotate('+deg+'deg)';
+    degLeft += 360;
+    leftRound.style.transform = "rotate(" + degLeft + "deg)";
+    article[0].classList.add("flexThree");
     article[1].classList.remove("flexThree");
+    leftText.style.display = "block";
+    rightText.style.display = "none";
   }
-})
+});
+
+article[1].addEventListener("mouseover", () => {
+  if (article[1].classList == "rightSide") {
+    degRight = degRight - 360;
+    rightRound.style.transform = "rotate(" + degRight + "deg)";
+    article[1].classList.add("flexThree");
+    article[0].classList.remove("flexThree");
+    rightText.style.display = "block";
+    leftText.style.display = "none";
+  }
+});
